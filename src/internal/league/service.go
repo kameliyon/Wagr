eca6ed88675c55dd2e0ebcb8600353035b2377f7
Aarron Hulswitt <aarron.hulswitt@gmail.com>
2026-01-31 19:10:14 -0500

    Starting the League import endpoints

---
+++ src/internal/league/service.go
@@ -0,0 +1,19 @@
+package league
+
+import (
+	"wagr/src/internal/fantasy/sleeper"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Service struct {
+	db *pgxpool.Pool
+	sleeperClient *sleeper.Client
+}
+
+func NewService(db *pgxpool.Pool, sleeperClient *sleeper.Client) *Service {
+	return &Service{
+		db: db,
+		sleeperClient: sleeperClient,
+	}
+}
